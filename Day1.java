ecfa8b4cad0ffa1846da996b6e1639b9d1b49587
Ghansyam <ghansyam@ubuntu.ubuntu-domain>
2013-05-13 23:36:51 +0530

    sent

--- Day1.java
+++ Day1.java
@@ -3,7 +3,7 @@ package com;
 public enum Day1 //enum define public only its own package or own class
 {				//becoz max one class should be public
 
-	SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+	SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY
 
 }
 
