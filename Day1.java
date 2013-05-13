b614ff07cea04b91e74798b801aaada0c36400d4
Ghansyam <ghansyam@ubuntu.ubuntu-domain>
2013-05-13 22:24:45 +0530

    added some test file

---
+++ Day1.java
@@ -0,0 +1,9 @@
+package com;
+//use other package
+public enum Day1 //enum define public only its own package or own class
+{				//becoz max one class should be public
+
+	SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+
+}
+
